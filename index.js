275b2d06aca27c3f658288d7753d01ecf8e61c8b
Diego Gallovich <dieguiviti@gmail.com>
2022-09-01 13:22:14 +0200

    testing svg circle attribute changes onTick event of timer instance

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
+
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let currentOffset = 0;
 
 const timer = new Timer(
   durationInput,
@@ -11,7 +17,11 @@ const timer = new Timer(
       console.log('Timer started');
     },
     onTick: () => {
-      console.log('Timer tick');
+      circle.setAttribute(
+        'stroke-dashoffset',
+        currentOffset
+      );
+      currentOffset -= 50;
     },
     onComplete: () => {
       console.log('Timer complete');
